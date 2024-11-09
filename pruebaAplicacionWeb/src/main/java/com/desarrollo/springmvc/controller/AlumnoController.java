6366149521cd53d5d3b1f8c29fb1d57174f6fb19
jcllt10 <llaczajc10@gmail.com>
2024-11-09 12:41:22 -0500

    segundo commit, se modifico la entidad alumno y se agrego el controlador AlumnoController

---
+++ pruebaAplicacionWeb/src/main/java/com/desarrollo/springmvc/controller/AlumnoController.java
@@ -0,0 +1,14 @@
+package com.desarrollo.springmvc.controller;
+
+import com.desarrollo.springmvc.entity.Alumno;
+
+public class AlumnoController {
+   
+    public Alumno registrarAlumno(Alumno alumno)
+    {
+        Alumno alumnoReg = new Alumno();
+        
+        return alumnoReg;
+    }
+    
+}
--- pruebaAplicacionWeb/src/main/java/com/desarrollo/springmvc/entity/Alumno.java
+++ pruebaAplicacionWeb/src/main/java/com/desarrollo/springmvc/entity/Alumno.java
@@ -2,4 +2,42 @@ package com.desarrollo.springmvc.entity;
 
 public class Alumno {
     
+    private String codigo;
+    private String nombre;
+    private String apellidos;
+
+    public Alumno() {
+    }
+
+    public Alumno(String codigo, String nombre, String apellidos) {
+        this.codigo = codigo;
+        this.nombre = nombre;
+        this.apellidos = apellidos;
+    }
+    
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+    
+    
 }
